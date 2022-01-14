2f1585f411d45ee80fe7c3205109e03a4f154d31
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-14 16:04:33 +0800

    feat: Create localStorage.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 import Header from "./Header";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     setContacts([...contacts, contact]);
-    console.log(contact);
   };
+
+  //Setting up the data to local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  //Getting the data from the local storage
+  useEffect(() => {
+    const getLocalData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (getLocalData) setContacts(getLocalData);
+  }, []);
 
   return (
     <div className="ui container">
