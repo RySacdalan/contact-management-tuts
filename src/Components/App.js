deb444f4167de2b9f74f29b0c1bc4c2fab7d04a7
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-14 16:17:12 +0800

    Install uuidv4

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 import Header from "./Header";
@@ -8,19 +9,20 @@ function App() {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuidv4(), ...contact }]);
   };
+
+  //Getting the data from the local storage
+  //getItem is on top of setItem code block.
+  useEffect(() => {
+    const getLocalData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (getLocalData) setContacts(getLocalData);
+  }, []);
 
   //Setting up the data to local storage
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
-
-  //Getting the data from the local storage
-  useEffect(() => {
-    const getLocalData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (getLocalData) setContacts(getLocalData);
-  }, []);
 
   return (
     <div className="ui container">
