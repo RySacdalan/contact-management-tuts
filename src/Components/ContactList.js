550c0bba8548021b70e974595c9da1bb977dedbb
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-19 12:48:47 +0800

    Add search bar.

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactCard from "./ContactCard";
 import { Link } from "react-router-dom";
+import "./App.css";
 
 const ContactList = ({ contacts, removeContactId }) => {
   const deleteContact = (id) => {
@@ -25,6 +26,12 @@ const ContactList = ({ contacts, removeContactId }) => {
           <button className="ui button blue right">Add Contact</button>
         </Link>
       </h1>
+      <div className="ui search">
+        <div className="ui icon input">
+          <input type="text" placeholder="Search Contact" className="prompt" />
+          <i className="search icon"></i>
+        </div>
+      </div>
       <div>{renderContactList}</div>
     </div>
   );
